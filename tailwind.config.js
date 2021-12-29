a8b1e887736567620a51b97f953bab364a706f12
uzairr <mohduzair1990@gmail.com>
2021-12-30 02:12:01 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    color:{
+      darkbrown:'#0f171e',
+   }
+  },
+  
+  plugins: [],
+}
